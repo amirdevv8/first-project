ade824b8f1618e7dba1b738596f48fd5d89f2174
amirdevv8 <167899101+amirdevv8@users.noreply.github.com>
2024-04-28 10:08:53 +0430

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+let bodyElem = document.querySelector("body");
+let menu = document.querySelector(".menu");
+let menuBtn = document.querySelector(".header__icon");
+let menuBtnIcon = document.querySelector(".header__icon i");
+let menuFlag = false;
+
+bodyElem.addEventListener("click", () => {
+  if (menuFlag) {
+    menu.style.left = "-256px";
+    menuBtnIcon.classList = "fa_fa-bars";
+    menuFlag = false;
+  }
+});
+
+menuBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (!menuFlag) {
+    menu.style.left = "0";
+    menuBtnIcon.classList = "fa_fa-times";
+    menuFlag = true;
+  }
+});
+
+menu.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
